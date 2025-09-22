1c32375d442c502a88e6ba033ffa097cbe59f7a3
Harsh Sutariya <hr.sutariya1@gmail.com>
2025-09-22 11:36:23 +0530

    fix: Update database password in db_config.php for proper connection

--- Website 1/db/db_config.php
+++ Website 1/db/db_config.php
@@ -3,7 +3,7 @@
 // Database credentials
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 define('DB_NAME', 'bytebazaar_db');
 
 // Attempt to connect to MySQL database
