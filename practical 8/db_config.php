1fc244a45503329b28d46f055e67699ec8450c57
Harsh Sutariya <hr.sutariya1@gmail.com>
2025-10-02 16:00:56 +0530

    including practicals 7,8

---
+++ practical 8/db_config.php
@@ -0,0 +1,20 @@
+<?php
+
+// Database credentials
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'bytebazaar_db');
+
+// Attempt to connect to MySQL database
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if ($conn->connect_error) {
+    die("ERROR: Could not connect. " . $conn->connect_error);
+}
+
+// Set the character set to utf8mb4 for full Unicode support
+$conn->set_charset("utf8mb4");
+
+?>
